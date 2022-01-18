d701dc759144eddedfe0eed1d9795db5dde5defa
Somayeh Ebrahimi <somayeh.ebrahimi68@gmail.com>
2022-01-18 17:40:04 +0330

    Initial files

---
+++ src/components/Card.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+    <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title>Title</v-card-title>
+
+    <v-card-text>
+      <div>Description</div>
+    </v-card-text>
+
+    <v-card-text>
+      <v-chip-group
+        v-model="selection"
+        active-class="deep-purple accent-4 white--text"
+        column
+      >
+        <v-chip>Tag</v-chip>
+        <v-chip>Tag</v-chip>
+        <v-chip>Tag</v-chip>
+        <v-chip>Tag</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Evan You</v-list-item-title>
+          <div>Jan</div>
+        </v-list-item-content>
+
+        <v-row align="center" justify="end">
+          <span class="subheading mr-2">256</span>
+          <v-icon class="mr-1"> mdi-heart </v-icon>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+    <!-- <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2"
+        text
+        @click="reserve"
+      >
+        Reserve
+      </v-btn>
+    </v-card-actions> -->
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row
+      class="p-2"
+      align="center"
+      justify="space-around"
+    >
+      <v-text-field
+        outlined
+        single-line
+        rounded
+        label="Prepend inner"
+        prepend-inner-icon="mdi-map-marker"
+      ></v-text-field>
+      <v-btn
+        tile
+        color="info"
+      >
+        <v-icon right>
+          mdi-pencil
+        </v-icon>
+        Add new post
+      </v-btn>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="n in 20"
+        :key="n"
+        cols="3"
+      >
+        <Card></Card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Card from '../components/Card'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      Card,
+    },
   }
-}
 </script>
